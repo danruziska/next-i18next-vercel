42a2532c3636f1b57a07ad98e640e6641a6272de
Danilo Ruziska <danilo.ruziska@closer.pt>
2021-08-15 02:48:46 +0100

    teste 3

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
+const path = require('path');
+
 module.exports = {
-  async rewrites() {
-    return [
-      [
-        {
-          source: "/carrinho/confirmacao",
-          destination: "/cart/checkout",
-        },
-        {
-          source: "/carrinho",
-          destination: "/cart",
-        },
-        {
-          source: "/esqueci-minha-senha",
-          destination: "/forgot-password",
-        },
-        {
-          source: "/login",
-          destination: "/entrar",
-        },
-        {
-          source: "/produtos",
-          destination: "/products",
-        },
-        {
-          source: "/registro",
-          destination: "/register",
-        }
-    ]
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
